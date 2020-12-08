6815f85ed2dd4b8e99e402dcf484200b6dedbe84
karina san roman <kds5612@psu.edu>
2020-12-08 12:33:27 -0500

    Created new controllers for each view

---
+++ src/controller/SearchController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author Karina
+ */
+public class SearchController {
+     @FXML
+    private Button searchButton;
+    
+    @FXML
+    private Button signOutButton;
+    
+    @FXML
+    private TextField searchField; 
+    
+    @FXML
+    private void search (ActionEvent event) {
+      //search by username 
+    }
+    
+    @FXML
+    private void signOut (ActionEvent event){
+        
+    }
+    
+    //need sign out method 
+    
+}
