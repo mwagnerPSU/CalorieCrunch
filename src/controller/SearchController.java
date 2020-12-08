4291a89ee7f5d5dc35f7a842411899a32b272cd7
mwagnerPSU <73369711+mwagnerPSU@users.noreply.github.com>
2020-12-08 13:39:30 -0500

    second use case

--- src/controller/SearchController.java
+++ src/controller/SearchController.java
@@ -5,35 +5,67 @@
  */
 package controller;
 
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
  * @author Karina
  */
 public class SearchController {
-     @FXML
+
+    @FXML
     private Button searchButton;
-    
+
     @FXML
     private Button signOutButton;
-    
+
     @FXML
-    private TextField searchField; 
+    private TextField searchField;
     
+    public String dropDownValue = "Personal Trainer";
+
     @FXML
-    private void search (ActionEvent event) {
-      //search by username 
-    }
-    
-    @FXML
-    private void signOut (ActionEvent event){
+    private void search(ActionEvent event) throws IOException {
+        //search by username 
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/clientDataView.fxml"));
+            Parent SearchScreen = loader.load();
+            Scene tableViewScene = new Scene(SearchScreen);
+
+            Stage stage = new Stage();
+            stage.setScene(tableViewScene);
+            stage.show();
         
+        /*
+        if (dropDownValue.equals("Personal Trainer")) {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/clientDataView.fxml"));
+            Parent SearchScreen = loader.load();
+            Scene tableViewScene = new Scene(SearchScreen);
+
+            Stage stage = new Stage();
+            stage.setScene(tableViewScene);
+            stage.show();
+        } else if (dropDownValue.equals("Medical Professional")) {
+            //put screen to go to
+        }
+        */
     }
-    
-    //need sign out method 
-    
+
+    @FXML
+    private void signOut(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/LoginPage.fxml"));
+        Parent SearchScreen = loader.load();
+        Scene tableViewScene = new Scene(SearchScreen);
+
+        Stage stage = new Stage();
+        stage.setScene(tableViewScene);
+        stage.show();
+    }
 }
