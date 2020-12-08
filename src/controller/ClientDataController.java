c547c90985a539b3c667ad223ca226d6f8ee4832
mwagnerPSU <73369711+mwagnerPSU@users.noreply.github.com>
2020-12-08 13:33:30 -0500

    second use case

--- src/controller/ClientDataController.java
+++ src/controller/ClientDataController.java
@@ -5,53 +5,69 @@
  */
 package controller;
 
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
  * @author Karina
  */
 public class ClientDataController {
-    
+
     @FXML
-    private TextField setGoalField; 
-    
+    private TextField setGoalField;
+
     @FXML
-    private TextField calorieViewField; 
-    
+    private TextField calorieViewField;
+
     @FXML
-    private TextField sendMesage; 
-    
+    private TextField sendMessage;
+
     @FXML
     private Button setGoalButton;
-    
+
     @FXML
     private Button viewCaloriesButton;
-    
+
     @FXML
     private Button sendMessageButton;
-    
+
     @FXML
     private Button signOutButton3;
-    
-    
+
     @FXML
-    private void viewCalories (ActionEvent event) {
-      //view the clients current calorie intake
+    private void viewCalories(ActionEvent event) {
+        //view the clients current calorie intake
+        calorieViewField.setText("1000");
     }
-    
+
     @FXML
-    private void setGoal (ActionEvent event) {
-      //allow trainer to set clients goal 
+    private void setGoal(ActionEvent event) {
+        //allow trainer to set clients goal 
     }
-    
+
     @FXML
-    private void sendMessage (ActionEvent event) {
-      //send client a motivational message 
+    private void sendMessage(ActionEvent event) {
+        //send client a motivational message 
     }
-    
+
     //sign out method
+    @FXML
+    private void signOut(ActionEvent event) throws IOException {
+        //send client a motivational message 
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/LoginPage.fxml"));
+        Parent LoginScreen = loader.load();
+        Scene tableViewScene = new Scene(LoginScreen);
+
+        Stage stage = new Stage();
+        stage.setScene(tableViewScene);
+        stage.show();
+    }
 }
