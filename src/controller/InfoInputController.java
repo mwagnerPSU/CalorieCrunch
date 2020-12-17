310d6b07e44498acfb7e30fc306477fe0ec7c0ac
mwagnerPSU <73369711+mwagnerPSU@users.noreply.github.com>
2020-12-16 19:25:51 -0500

    getting back and forth between database working

--- src/controller/InfoInputController.java
+++ src/controller/InfoInputController.java
@@ -6,11 +6,15 @@
 package controller;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Date;
 import java.util.List;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -18,14 +22,16 @@ import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
 import javax.persistence.Query;
 import model.History;
+import model.Person;
 
 /**
  *
  * @author Karina
  */
-public class InfoInputController {
+public class InfoInputController implements Initializable{
     @FXML
     private Button checkGoalButton;
     
@@ -39,33 +45,37 @@ public class InfoInputController {
     private Button totalCaloriesButton;
     
     @FXML
-    private TextField intakeGoalField = new TextField();
+    private TextField intakeGoalField;
     @FXML
     private TextField recentIntakeField;
     @FXML
     private Text totalCaloriesText;
     
-    EntityManager manager;
-    
-     @FXML
+    @FXML
     private Text goalText = new Text();
     
     @FXML
     private Button totalCals;
-    
-    public String calorieHolder = "Hello";
-    
-    public LoginPageController loginCont = new LoginPageController();
-    
-    //public String currentUsername;
 
-//    public void setCurrentUsername(String currentUsername) {
-//        this.currentUsername = currentUsername;
-//    }
-//    
+    String currentUser;
+    String credentials;
     
+    public void initData(String username, String creds){
+        currentUser = username;
+        credentials = creds;
+        
+        System.out.println("Welcome " + currentUser);
+        System.out.println("Creds: " + credentials);
+    }
     
-    public void create(History newHistory) {
+    EntityManager manager;
+    public void initialize(URL url, ResourceBundle rb) {
+        
+        manager = (EntityManager) Persistence.createEntityManagerFactory("CalorieCrunchFXMLPU").createEntityManager();
+        
+    }
+    
+    public void createHistory(History newHistory) {
         try {
             // begin transaction
             manager.getTransaction().begin();
@@ -86,35 +96,112 @@ public class InfoInputController {
         }
     }
     
-    
-        @FXML
-    public void saveIntakeGoal(ActionEvent event) {
-        //save the goal they set to their name and ID in the table
-        calorieHolder = intakeGoalField.getText();
-        System.out.println(calorieHolder + "save");
-            System.out.println(loginCont.getCurrentUsername());
-            //System.out.println(currentUsername);
-        
-        History newHistory = new History();
-        
-        newHistory.setId(readAll().size() + 1);
-        newHistory.setUsername(loginCont.getCurrentUsername());
-//        newHistory.setFirstname(fName);
-//        newHistory.setLastname(lName);
-        
-        create(newHistory);
-        
+    public void updatePerson(Person model) {
+        try {
+
+            Person existingPerson = manager.find(Person.class, model.getUsername());
+
+            if (existingPerson != null) {
+                // begin transaction
+                manager.getTransaction().begin();
+                
+                // update current calories
+                existingPerson.setCurrentcalories(model.getCurrentcalories());
+                existingPerson.setCurrentgoal(model.getCurrentgoal());
+                
+                // end transaction
+                manager.getTransaction().commit();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
     }
     
+    
     @FXML
-    private void setCalText(ActionEvent event){
-        totalCaloriesText.setText("1000");
+    public void saveIntakeGoal(ActionEvent event) {
+        //save the goal they set to their name and ID in the table
+        System.out.println("Save");
+        
+        int idSize = readAll().size() + 1;
+        int currCals = readByUsername(currentUser).getCurrentcalories();
+
+        History newHistory = new History();
+        
+        newHistory.setId(idSize);
+        newHistory.setUsername(currentUser);
+        newHistory.setDate(new Date());
+        newHistory.setCurrentgoal(Integer.parseInt(intakeGoalField.getText()));
+        newHistory.setCurrentcalories(currCals);
+        
+        createHistory(newHistory);
+        
+        
+        int idPerson = readByUsername(currentUser).getId();
+        String password = readByUsername(currentUser).getPassword();
+        String currCredentials = readByUsername(currentUser).getCredentials();
+        int currCalsPerson = readByUsername(currentUser).getCurrentcalories();
+        int currGoal = Integer.parseInt(intakeGoalField.getText());
+        
+        Person updatedPerson = new Person();
+        
+        updatedPerson.setId(idPerson);
+        updatedPerson.setUsername(currentUser);
+        updatedPerson.setPassword(password);
+        updatedPerson.setCredentials(currCredentials);
+        updatedPerson.setCurrentcalories(currCalsPerson);
+        updatedPerson.setCurrentgoal(currGoal);
+        
+        updatePerson(updatedPerson);
     }
     
     @FXML
     private void updateIntake(ActionEvent event) {
-        // update their current intake in the table 
+        System.out.println("update");
+        
+        // update their current intake in the history and person table 
+        int idSize = readAll().size() + 1;
+        int currCals = readByUsername(currentUser).getCurrentcalories();
+        int currGoal = readByUsername(currentUser).getCurrentgoal();
+        
+        History newHistory = new History();
+        newHistory.setId(idSize);
+        newHistory.setUsername(currentUser);
+        newHistory.setDate(new Date());
+        newHistory.setCurrentgoal(currGoal);
+        newHistory.setCurrentcalories(currCals);
+        
+        createHistory(newHistory);
+        
+        int idPerson = readByUsername(currentUser).getId();
+        String password = readByUsername(currentUser).getPassword();
+        String currCredentials = readByUsername(currentUser).getCredentials();
+        int oldCals = Integer.parseInt(readByUsernameP(currentUser));
+        int newCals = Integer.parseInt(recentIntakeField.getText());
+        int currCalsPerson = oldCals + newCals;
+        int currGoalP = readByUsername(currentUser).getCurrentgoal();
+        
+        Person updatedPerson = new Person();
+        
+        updatedPerson.setId(idPerson);
+        updatedPerson.setUsername(currentUser);
+        updatedPerson.setPassword(password);
+        updatedPerson.setCredentials(currCredentials);
+        updatedPerson.setCurrentcalories(currCalsPerson);
+        updatedPerson.setCurrentgoal(currGoalP);
+        
+        updatePerson(updatedPerson);
     }
+    
+    @FXML
+    private void setCalText(ActionEvent event){
+        System.out.println("set cal");
+        
+        //save new intake goal to history table
+        
+        totalCaloriesText.setText(readByUsernameP(currentUser));
+    }
+    
     @FXML
     private void signOut(ActionEvent event) throws IOException {
        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/LoginPage.fxml")); 
@@ -144,49 +231,42 @@ public class InfoInputController {
        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/GoalSummaryPage.fxml")); 
        Parent GoalSummaryScreen = loader.load(); 
        Scene tableViewScene = new Scene(GoalSummaryScreen);
+       GoalSummaryController goalSumCont = loader.getController();
+       goalSumCont.initData(currentUser);
        
        Stage stage = new Stage();
        stage.setScene(tableViewScene);
        stage.show();
     }
     
-//     public void update(History model) {
-//        try {
-//
-//            MedicalProfessionalModel existingMedicalProfessional = manager.find(MedicalProfessionalModel.class, model.getId());
-//
-//            if (existingMedicalProfessional != null) {
-//                // begin transaction
-//                manager.getTransaction().begin();
-//                
-//                // update all atttributes
-//                existingMedicalProfessional.setFirstname(model.getFirstname());
-//                existingMedicalProfessional.setLastname(model.getLastname());
-//                existingMedicalProfessional.setCredentials(model.getCredentials());
-//                
-//                // end transaction
-//                manager.getTransaction().commit();
-//            }
-//        } catch (Exception ex) {
-//            System.out.println(ex.getMessage());
-//        }
-//    }
-    
-    
-    public History readByUsername(String username){
-        Scanner userInput = new Scanner(System.in);
-        Query query = manager.createNamedQuery("History.findByUsername");
+    public Person readByUsername(String username){
+        Query query = manager.createNamedQuery("Person.findByUsername");
         
         // setting query parameter
         query.setParameter("username", username);
         
         // execute query
-        History result = (History) query.getSingleResult();
+        Person result = (Person) query.getSingleResult();
         if (result != null){
-            System.out.println("Id: " + result.getId() + " | Username: " + result.getUsername() + " | Date" + result.getDate() + " | Current Goal: " + result.getCurrentgoal() + " | Current Calories: " + result.getCurrentcalories());
+            System.out.println("Id: " + result.getId() + " | Username: " + result.getUsername() + " | Password" + result.getPassword() + " | Credentials: " + result.getCredentials() + " | Current Calories: " + result.getCurrentcalories());
         }
         
         return result;        
+    }
+    
+    public String readByUsernameP(String username){
+        Query query = manager.createNamedQuery("Person.findByUsername");
+        
+        // setting query parameter
+        query.setParameter("username", username);
+        
+        // execute query
+        Person result = (Person) query.getSingleResult();
+        if (result != null){
+            System.out.println("Id: " + result.getId() + " | Username: " + result.getUsername() + " | Password" + result.getPassword() + " | Credentials: " + result.getCredentials() + " | Current Calories: " + result.getCurrentcalories());
+        }
+        
+        return result.getCurrentcalories().toString();        
     }
     
     public List<History> readAll(){
@@ -198,5 +278,16 @@ public class InfoInputController {
         }
         
         return history;
+    }
+    
+    public List<Person> readAllPerson(){
+        Query query = manager.createNamedQuery("Person.findAll");
+        List<Person> people = query.getResultList();
+
+        for (Person p : people) {
+            System.out.println("ID: " + p.getId() + " | Name: " + p.getUsername() + " | Password:" + p.getPassword() + " | Credentials: " + p.getCredentials() + " | Current Calories: " + p.getCurrentcalories());
+        }
+        
+        return people;
     }
 }
